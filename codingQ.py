15d3d7dcaec674916bf98d6106d97855db0f2859
TSOT5 <147224452+TSOT5@users.noreply.github.com>
2023-10-10 19:14:40 -0500

    Create codingQ.py
    homework

---
+++ codingQ.py
@@ -0,0 +1,31 @@
+#Question 1
+
+def hello_name(user_name):
+    user_name = input("What is your name? ")
+    print ("hello_",user_name + "!")
+
+#Question 2
+
+def first_odds():
+    for x in range (1,101,2):
+        print (x)
+    return
+    
+#Question 3
+
+def max_num_in_list(a_list):
+    number = max(a_list)
+    return number
+
+#Question 4
+
+def is_leap_year(a_year):
+    if a_year % 4 == 0 and a_year % 100 != 0 or a_year % 400 == 0:
+            return True
+    return False
+
+#Question 5
+
+def is_consecutive(a_list):
+    
+    return a_list == list(range(a_list[0], a_list[-1] + 1))
